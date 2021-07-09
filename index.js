574ce18816a9ba62e26db31b76b1e05845068c4e
MostafaOsama223 <mostafaosama030@gmail.com>
2021-07-09 02:19:44 +0200

    [ADD] dotenv and app-root-path modules. [REF] importing models in a dynamic way, check db connection.

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
+require('dotenv').config();
+
 const sequelize = require('./Utils/database');
 
-/*  MODELS  */
-const Doctor = require('./Models/doctor');
-const DrPatient = require('./Models/drPatient');
-const Game = require('./Models/game');
-const Injury = require('./Models/injury');
-const Level = require('./Models/level');
-const Patient = require('./Models/patient');
-const PatientGame = require('./Models/patientGame');
-const Progress = require('./Models/progress');
-
-sequelize.authenticate()
-    .then(res => { console.log("db connected"); main() })
-    .catch(rej => console.log(rej));
-
-
-function main() {
-    
+(() => {
     sequelize.sync({
         force: false
     });
-    Game.create({gameName: "game1", gameUrl: 'xxxx'})
-    Level.create({timeSpent: '25:16:20', score: 32, maxScore: 30, difficulty: 'easy', gameId: 1})
+    // Game.create({gameName: "game1", gameUrl: 'xxxx'})
+    // Level.create({timeSpent: '25:16:20', score: 32, maxScore: 30, difficulty: 'easy', gameId: 1})
 
-}
+})();
